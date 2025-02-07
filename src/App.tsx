74ab320845e92af9ab69427197a5c5c23ad33704
melki <melkishitesfayeangassa@gmail.com>
2025-02-07 14:06:54 +0300

    Build a reponsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,39 @@
-import { Button, HStack } from "@chakra-ui/react"
+import { Grid, GridItem, Show, useBreakpointValue } from "@chakra-ui/react";
 
 function App() {
-
+  const isLargeScreen = useBreakpointValue({ base: false, lg: true });
   return (
-    <>
-     <HStack>
-      <Button>Click me</Button>
-      <Button>Click me</Button>
-    </HStack>
-    </>
-  )
+    <Grid
+      templateAreas={{
+        base: `
+          "header"
+          "main"
+          "footer"
+        `,
+        lg: `
+          "header header"
+          "main aside"
+          "footer footer"
+        `,
+      }}
+    >
+      <GridItem area="header" bg="coral">
+        Header
+      </GridItem>
+      <GridItem area="main" bg="blue">
+        Main
+      </GridItem>
+      <Show when={isLargeScreen}>
+
+      <GridItem area="aside" bg="green">
+        Aside
+      </GridItem>
+      </Show>
+      <GridItem area="footer" bg="purple">
+        Footer
+      </GridItem>
+    </Grid>
+  );
 }
 
-export default App
+export default App;
