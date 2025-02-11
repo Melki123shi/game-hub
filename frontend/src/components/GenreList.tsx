d8ec34c968e888cdd8213f432d5670c7437a5553
melki <melkishitesfayeangassa@gmail.com>
2025-02-11 22:05:50 +0300

    Fetch genre

---
+++ frontend/src/components/GenreList.tsx
@@ -0,0 +1,12 @@
+import UseGenres from "@/hooks/UseGenres";
+
+const GenreList = () => {
+    const {genres} = UseGenres();
+  return (
+    <ul>
+        {genres.map(genre => <li key={genre.id}>{genre.name}</li> )}
+    </ul>
+  )
+}
+
+export default GenreList
