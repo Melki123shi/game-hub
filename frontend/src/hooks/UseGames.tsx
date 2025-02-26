2364cf346f8cab67b4c63f8249972f49234861b9
melki <melkishitesfayeangassa@gmail.com>
2025-02-26 09:41:53 +0300

    change to ractquery

--- frontend/src/hooks/UseGames.tsx
+++ frontend/src/hooks/UseGames.tsx
@@ -1,5 +1,7 @@
 import { GameQuery } from "@/App";
-import { UseData } from "./UseData";
+import { GridResponse } from "./UseData";
+import { useQuery } from "@tanstack/react-query";
+import apiClient from "@/services/api-client";
 
 export interface Game {
   _id: string;
@@ -13,15 +15,19 @@ export interface Game {
 }
 
 export const useGames = (gameQuery: GameQuery) =>
-  UseData<Game>(
-    "/games",
-    {
-      params: {
-        genre: gameQuery.genre?.name,
-        platform: gameQuery.platform?.name,
-        sortBy: gameQuery.sortBy,
-        search: gameQuery.searchText,
-      },
-    },
-    [gameQuery]
-  );
+  useQuery<Game[], Error>({
+    queryKey: ["games", gameQuery],
+    queryFn: () =>
+      apiClient.get<GridResponse<Game>>(
+        "/games",
+        {
+          params: {
+            genre: gameQuery.genre?.name,
+            platform: gameQuery.platform?.name,
+            sortBy: gameQuery.sortBy,
+            search: gameQuery.searchText,
+          },
+        }
+      ).then((res) => res.data.results || []),
+    staleTime: 10 * 1000,
+  });
